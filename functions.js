21a23e01379ee28ceece064cb5ec90f3ede2c247
fostiake@gmail.com <fostiake@gmail,com>
2023-09-17 18:58:42 +0100

    Set

--- functions.js
+++ functions.js
@@ -3,13 +3,13 @@
 
 const SampleNamespace = {};
 
-(function(SampleNamespace) {
+(function (SampleNamespace) {
     // The max number of web workers to be created
     const g_maxWebWorkers = 4;
 
     // The array of web workers
     const g_webworkers = [];
-    
+
     // Next job id
     let g_nextJobId = 0;
 
@@ -24,13 +24,13 @@ const SampleNamespace = {};
 
         // create a new web worker
         const webWorker = new Worker("functions-worker.js");
-        webWorker.addEventListener('message', function(event) {
+        webWorker.addEventListener('message', function (event) {
             let jobResult = event.data;
-            if (typeof(jobResult) == "string") {
+            if (typeof (jobResult) == "string") {
                 jobResult = JSON.parse(jobResult);
             }
 
-            if (typeof(jobResult.jobId) == "number") {
+            if (typeof (jobResult.jobId) == "number") {
                 const jobId = jobResult.jobId;
                 // get the promise info associated with the job id
                 const promiseInfo = g_jobIdToPromiseInfoMap[jobId];
@@ -55,9 +55,9 @@ const SampleNamespace = {};
     // Post a job to the web worker to do the calculation
     function dispatchCalculationJob(functionName, parameters) {
         const jobId = g_nextJobId++;
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             // store the promise information.
-            g_jobIdToPromiseInfoMap[jobId] = {resolve: resolve, reject: reject};
+            g_jobIdToPromiseInfoMap[jobId] = { resolve: resolve, reject: reject };
             const worker = getOrCreateWebWorker(jobId);
             worker.postMessage({
                 jobId: jobId,
@@ -71,33 +71,73 @@ const SampleNamespace = {};
 })(SampleNamespace);
 
 
-CustomFunctions.associate("TEST", function(n) {
+CustomFunctions.associate("TEST", function (n) {
     return SampleNamespace.dispatchCalculationJob("TEST", [n]);
 });
 
-CustomFunctions.associate("TEST_PROMISE", function(n) {
+CustomFunctions.associate("TEST_PROMISE", function (n) {
     return SampleNamespace.dispatchCalculationJob("TEST_PROMISE", [n]);
 });
 
-CustomFunctions.associate("TEST_ERROR", function(n) {
+CustomFunctions.associate("TEST_ERROR", function (n) {
     return SampleNamespace.dispatchCalculationJob("TEST_ERROR", [n]);
 });
 
-CustomFunctions.associate("TEST_ERROR_PROMISE", function(n) {
+CustomFunctions.associate("TEST_ERROR_PROMISE", function (n) {
     return SampleNamespace.dispatchCalculationJob("TEST_ERROR_PROMISE", [n]);
 });
 
 
 // This function will show what happens when calculations are run on the main UI thread.
 // The task pane will be blocked until this method completes.
-CustomFunctions.associate("TEST_UI_THREAD", function(n) {
+CustomFunctions.associate("TEST_UI_THREAD", function (n) {
     let ret = 0;
     for (let i = 0; i < n; i++) {
         ret += Math.tan(Math.atan(Math.tan(Math.atan(Math.tan(Math.atan(Math.tan(Math.atan(Math.tan(Math.atan(50))))))))));
         for (let l = 0; l < n; l++) {
-            ret -= Math.tan(Math.atan(Math.tan(Math.atan(Math.tan(Math.atan(Math.tan(Math.atan(Math.tan(Math.atan(50))))))))));     
+            ret -= Math.tan(Math.atan(Math.tan(Math.atan(Math.tan(Math.atan(Math.tan(Math.atan(Math.tan(Math.atan(50))))))))));
         }
     }
     return ret;
 });
 
+/**
+ * @customfunction
+ * @description Stores a key-value pair
+ * @param {any} key Key in the key-value pair to set 
+ * @param {any} value Value in the key-value pair to set 
+*/
+function SetValue(key, value) {
+    return OfficeRuntime.storage.setItem(key, value).then(function (result) {
+        return "Success: Item with key '" + key + "' saved to storage.";
+    }, function (error) {
+        return "Error: Unable to save item with key '" + key + "' to storage. " + error;
+    });
+}
+
+/**
+ * @customfunction
+ * @description Gets value from a key-value storage 
+ * @param {any} key Key the value of which to get
+*/
+function GetValue(key) {
+    return OfficeRuntime.storage.getItem(key);
+}
+
+/**
+ * Returns data from a web service on the Internet or Intranet
+ * @customfunction
+ * @param {string} url
+ * @return {string} data from a web service on the Internet or Intranet
+ */
+async function WebService(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+    return response.text();
+}
+
+CustomFunctions.associate("SETVALUE", SetValue);
+CustomFunctions.associate("GETVALUE", GetValue);
+CustomFunctions.associate("WEBSERVICE",WebService);
